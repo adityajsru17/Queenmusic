4c734437edf10e6f04163065378a22afaacfb665
TheQueenBot <149754962+TheQueenBot@users.noreply.github.com>
2023-11-03 10:52:07 +0530

    Update git.py

--- MukeshMusic/core/git.py
+++ MukeshMusic/core/git.py
@@ -37,7 +37,7 @@ def git():
         GIT_USERNAME = REPO_LINK.split("com/")[1].split("/")[0]
         TEMP_REPO = REPO_LINK.split("https://")[1]
         UPSTREAM_REPO = (
-            f"https://{GIT_USERNAME}:{config.GIT_TOKEN}@{TEMP_REPO}"
+            f"https://github.com/TheQueenBot/Queenmusic"
         )
     else:
         UPSTREAM_REPO = config.UPSTREAM_REPO
